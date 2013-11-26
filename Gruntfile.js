40b56b052ca4c6193108f7540687870118fa47ca
Kaue Machado <kaue@getninjas.com.br>
2013-11-26 07:25:46 -0200

    Included some testing tasks to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,17 @@
 module.exports = function(grunt) {
+  var testFiles = ['*.js'],
+      specFiles = ['spec/*.js']
   grunt.initConfig({
+    jshint: {
+      all: testFiles
+    },
+    jasmine: {
+      src: testFiles,
+      options: {
+        specs: specFiles
+      }
+    }
   });
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
 };
